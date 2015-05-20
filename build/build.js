54bbf80ecb2d3d11aa76c06a803efe974f7a0182
invisal <invisal@gmail.com>
2015-05-20 13:07:12 +0700

    Build: add Javascript minifier.
    Build: add Javascript minifier.

--- build/build.js
+++ build/build.js
@@ -1,8 +1,21 @@
+var _fs = require('fs');
 var shell = require('shelljs');
 
-shell.mkdir('dist');
+function uglify(srcPath, distPath) {
+    var jsp = require('uglify-js').parser;
+    var pro = require('uglify-js').uglify;
+    var ast = jsp.parse( _fs.readFileSync(srcPath).toString() );
+ 
+    ast = pro.ast_mangle(ast);
+    ast = pro.ast_squeeze(ast);
+ 
+    _fs.writeFileSync(distPath, pro.gen_code(ast));
+}
 
+shell.mkdir('dist');
 shell.cat([
 	'src/SharedWebSocket.js',
 	'src/ReconnectStrategy.js',
 ]).to('dist/websocket.js');
+
+uglify('dist/websocket.js', 'dist/websocket.min.js');
